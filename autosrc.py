08a3b658e49ab996e2db6230b2b6b8c40bed6f91
parieeeh <106758959+parieeeh@users.noreply.github.com>
2022-06-02 17:12:01 +0200

    Create autosrc.py

---
+++ autosrc.py
@@ -0,0 +1,27 @@
+from pynput.keyboard import Key, Controller
+import webbrowser
+import time
+keyboard = Controller()
+def split(words):
+        return [char for char in words]
+print(" ")
+print("AUTOSRC by parieeeh")
+print(" ")
+argument= input("what do you have to look for?")
+time.sleep(1)
+print(" ")
+print("I'm looking for it for you...")
+webbrowser.open("www.google.com")
+time.sleep(3)
+for i in split(argument):
+    keyboard.type(i)
+    time.sleep(.1)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    print(" ")
+print("My research ends here!")
+time.sleep(1)
+print(" ")
+print("-an idea of parieeeh-")
+time.sleep(2)
+#parieeeh idea's
